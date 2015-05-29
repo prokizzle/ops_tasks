16b9e45a5cb8b724067ca2e0139040add63f88dd
Nick Prokesch <nprokesch@gmail.com>
2015-05-29 14:32:10 -0500

    split classes into their own files

---
+++ lib/ops_tasks/rake_helper.rb
@@ -0,0 +1,42 @@
+require 'highline/import'
+
+module OpsTasks
+  class RakeHelper
+    def self.servers
+      ENV.keys.select do |k|
+        k.match(/_stack_id/) && !k.match(/FIGARO/)
+      end.map{|k| k.match(/(.+)_stack_id/)[1]}
+    end
+
+    def self.check_for_env_vars
+      if servers.empty?
+        puts "You haven't setup your layers in your environment variables"
+        exit
+      end
+    end
+
+    def self.select_server_type
+      check_for_env_vars
+      @server_type = nil
+      say("\nSelect a server...")
+      choose do |menu|
+        servers.each do |server|
+          menu.choice server do @server_type = server end
+        end
+        menu.choice "quit" do exit end
+      end
+      return @server_type
+    end
+
+    def self.create_deployment
+      server_type = select_server_type
+      return OpsTasks::Deployment.new(
+        layer_id: ENV["#{server_type}_layer_id"],
+        stack_id: ENV["#{server_type}_stack_id"],
+        recipe: ENV["#{server_type}_deploy_recipe"],
+        project: ENV["#{server_type}_project_name"],
+        room: ENV["#{server_type}_slack_channel"]
+      )
+    end
+  end
+end
