157900c0c3057151232bb9c705ccffac2ec336e0
Nick Prokesch <nick@prokes.ch>
2015-09-17 16:06:12 -0500

    Add ability to start instances on creation

--- lib/ops_tasks/scale.rb
+++ lib/ops_tasks/scale.rb
@@ -13,10 +13,27 @@ module OpsTasks
       )[:instance_id]
     end
 
+    def start_instance(instance_id)
+      @client.start_instance(
+        :instance_id => instance_id
+      )
+    end
+
     def assess_status(instance_id)
       @client.describe_instances(
         :instance_ids => [instance_id]
       )[:instances].first[:status]
+    end
+
+    def deployment_failed?(instance_id)
+      @client.describe_instances(
+        :instance_ids => [instance_id]
+      )[:instances].first[:status] == 'failed'
+    end
+
+    def poll_api_for_status(deployment_id)
+      sleep 1 until assess_status(deployment_id) == 'online'
+      puts assess_status(deployment_id)
     end
 
     def wait_for_completion(instance_id, task="create instance")
